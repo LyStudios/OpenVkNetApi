2a3d0fba228b413b71c0b1b68d290b514728d23d
Leenzery <63073463+LeenzeryDev@users.noreply.github.com>
2022-06-21 01:52:57 +0500

    Добавил метод Friends.Get

---
+++ Methods/Friends.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using OpenVkNetApi.Models;
+using OpenVkNetApi.Models.Friends;
+using System.Threading.Tasks;
+
+namespace OpenVkNetApi.Methods
+{
+    public class Friends
+    {
+        OVkApi api = new OVkApi();
+        public async Task<GetFriendsModel> Get(AuthorizedUser user, int user_id, string fields="", int offset=0, int count=100)
+        {
+            return JsonConvert.DeserializeObject<GetFriendsModel>(await api.GetRequestAsync($"{user.instanceUrl}/method/Friends.get?access_token={user.access_token}&user_id={user_id}&offset={offset}&count={count}&fields={fields}"));
+        }
+    }
+}
---
+++ Models/Friends/GetFriendsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenVkNetApi.Models.Friends
+{
+    public class GetFriendsModel
+    {
+        public GetFriendsResponse response { get; set; }
+    }
+    public class FriendItem
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public bool is_closed { get; set; }
+        public bool can_access_closed { get; set; }
+        public int online { get; set; }
+    }
+
+    public class GetFriendsResponse
+    {
+        public int count { get; set; }
+        public List<FriendItem> items { get; set; }
+    }
+}
