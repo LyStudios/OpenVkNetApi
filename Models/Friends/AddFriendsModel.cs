878de6a2bcab0010d9b504abdca973fe36d37666
Leenzery <ly@leenzery.dev>
2022-06-21 03:30:11 +0500

    Add files via upload

---
+++ Models/Friends/AddFriendsModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace OpenVkNetApi.Models.Friends
+{
+    public class AddFriendsModel 
+    {         
+        public int addFriendsResponse {get;set;}
+    }
+}
