0c977f599050c29d8128494b3f8824c02d766d4f
Leenzery <ly@leenzery.dev>
2022-06-21 03:31:36 +0500

    Добавил метод Friends.Add

--- Methods/Friends.cs
+++ Methods/Friends.cs
@@ -12,5 +12,8 @@ namespace OpenVkNetApi.Methods
         {
             return JsonConvert.DeserializeObject<GetFriendsModel>(await api.GetRequestAsync($"{user.instanceUrl}/method/Friends.get?access_token={user.access_token}&user_id={user_id}&offset={offset}&count={count}&fields={fields}"));
         }
+        public async Task<int> Add(AuthorizedUser user, int user_id){
+        	return JsonConvert.DeserializeObject<AddFriendsModel>(await api.GetRequestAsync($"{user.instanceUrl}/method/Friends.add?access_token={user.access_token}&user_id={user_id}"));
+        }
     }
 }
