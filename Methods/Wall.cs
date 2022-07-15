ba7056b3c44ab566eb341cfdfb5f06a519268d43
Leenzery <ly@leenzery.dev>
2022-07-15 22:27:34 +0500

    Дописал Wall, в теории оно вроде и готово, но нету документации
    А ещё блять постинг медиа выбивает ошибку 500

--- Methods/Wall.cs
+++ Methods/Wall.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using OpenVkNetApi.Models;
 using OpenVkNetApi.Models.Wall;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace OpenVkNetApi.Methods
@@ -11,6 +12,33 @@ namespace OpenVkNetApi.Methods
         public async Task<GetModel> Get(AuthorizedUser user, string owner_id, int extended = 0, int offset = 0, int count = 30)
         {
             return JsonConvert.DeserializeObject<GetModel>(await api.GetRequestAsync($"{user.instanceUrl}/method/Wall.get?access_token={user.access_token}&owner_id={owner_id}&extended={extended}&offset={offset}&count={count}"));
+        }
+        public async Task<GetByIdModel> GetById(AuthorizedUser user, string posts, int extended = 0, string fields = "")
+        {
+            return JsonConvert.DeserializeObject<GetByIdModel>(await api.GetRequestAsync($"{user.instanceUrl}/method/Wall.getById?access_token={user.access_token}&posts={posts}&extended={extended}&fields={fields}"));
+        }
+        public async Task Post(AuthorizedUser user, string owner_id, string contentType = "photo", byte[] file = null, string message = "", int from_group = 0, int signed = 0)
+        {
+            if (file == null) {
+                var content = new MultipartFormDataContent();
+                content.Add(new StringContent(user.access_token), "access_token");
+                content.Add(new StringContent(owner_id), "owner_id");
+                content.Add(new StringContent(message), "message");
+                content.Add(new StringContent(from_group.ToString()), "from_group");
+                content.Add(new StringContent(signed.ToString()), "signed");
+                var response = await new HttpClient().PostAsync($"{user.instanceUrl}/method/Wall.post", content);
+            }
+            else
+            { 
+                var content = new MultipartFormDataContent();
+                content.Add(new StringContent(user.access_token), "access_token");
+                content.Add(new StringContent(owner_id), "owner_id");
+                content.Add(new StringContent(message), "message");
+                content.Add(new StringContent(from_group.ToString()), "from_group");
+                content.Add(new StringContent(signed.ToString()), "signed");
+                content.Add(new ByteArrayContent(file), contentType);
+                var response = await new HttpClient().PostAsync($"{user.instanceUrl}/method/Wall.post", content);
+            }
         }
     }
 }
