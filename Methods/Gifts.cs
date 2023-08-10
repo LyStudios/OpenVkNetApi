a68009ff3fe749abd118977d2886bb936ec92d78
Leenzery <ly@leenzery.dev>
2023-08-11 02:02:27 +0500

    Подарочки

---
+++ Methods/Gifts.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using OpenVkNetApi.Models.GiftsModels;
+using System.Threading.Tasks;
+
+namespace OpenVkNetApi.Methods
+{
+    public class Gifts
+    {
+        private string AccessToken;
+        private string InstanceUrl;
+        OvkApi api = new OvkApi();
+        public Gifts(string token, string instance)
+        {
+            this.AccessToken = token;
+            this.InstanceUrl = instance;
+        }
+        public async Task<UserGift[]> Get(string User_Id, int Offset = 0, int Count = 10)
+        {
+            return JsonConvert.DeserializeObject<dynamic>(await api.GetRequest($"{InstanceUrl}/method/Gifts.set?access_token={AccessToken}&user_id={User_Id}&offset={Offset}&count={Count}")).response.ToObject(typeof(UserGift[]));
+        }
+        public async Task<Gift[]> GetGiftsInCategory(string Id, int Page = 1)
+        {
+            return JsonConvert.DeserializeObject<dynamic>(await api.GetRequest($"{InstanceUrl}/method/Gifts.getGiftsInCategory?access_token={AccessToken}&id={Id}&page={Page}")).response.ToObject(typeof(Gift[]));
+        }
+        public async Task<GiftCategory[]>GetCategories(int Extended = 0, int Page = 1)
+        {
+            return JsonConvert.DeserializeObject<dynamic>(await api.GetRequest($"{InstanceUrl}/method/Gifts.getCategories?access_token={AccessToken}&extended={Extended}&page={Page}")).response.ToObject(typeof(GiftCategory[]));
+        }
+        public async Task<SendResponse> Send(string User_Ids, int Gift_Id, string Message = "", int Privacy = 0)
+        {
+            return JsonConvert.DeserializeObject<dynamic>(await api.GetRequest($"{InstanceUrl}/method/Gifts.send?access_token={AccessToken}&user_ids={User_Ids}&gift_id={Gift_Id}&message={Message}&privacy={Privacy}")).response.ToObject(typeof(SendResponse));
+        }
+    }
+}
---
+++ Models/GiftsModels/Gifts.cs
@@ -0,0 +1,44 @@
+﻿namespace OpenVkNetApi.Models.GiftsModels
+{
+    public class UserGift
+    {
+        public int id { get; set; }
+        public int from_id { get; set; }
+        public string message { get; set; }
+        public int date { get; set; }
+        public SendedGift gift { get; set; }
+        public int privacy { get; set; }
+    }
+
+    public class SendedGift
+    {
+        public int id { get; set; }
+        public string thumb_256 { get; set; }
+        public string thumb_96 { get; set; }
+        public string thumb_48 { get; set; }
+    }
+
+    public class GiftCategory
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public int id { get; set; }
+        public string thumbnail { get; set; }
+    }
+
+    public class SendResponse
+    {
+        public int success { get; set; }
+        public int user_ids { get; set; }
+        public int withdraw_votes { get; set; }
+    }
+
+    public class Gift
+    {
+        public string name { get; set; }
+        public string image { get; set; }
+        public int usages_left { get; set; }
+        public int price { get; set; }
+        public bool is_free { get; set; }
+    }
+}
